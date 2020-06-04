037493c94095be1c2b31c26ffa6c9ce6714bb2ca
Yonni <yonni.palay@gmail.com>
2020-06-04 10:17:14 +0200

    Routing working

--- app.js
+++ app.js
@@ -1,15 +1,18 @@
-const http = require('http')
+// const http = require('http')
 const port = process.env.PORT || 3000
 // const port = 3005
+/*
 const server = http.createServer((req, res) => {
   res.statusCode = 200
   res.setHeader('Content-Type', 'text/html')
   res.end('<h1>Hello World</h1>')
 })
+*/
+/*
 server.listen(port, () => {
   console.log('Server running at port ' + port)
 })
-/*
+*/
 const express = require('express')
 const app = express()
 const mainRoutes = require('./mainRoutes')
@@ -21,4 +24,3 @@ app.listen(port)
 console.log('Express server running on port', port)
 
 console.log('index.js ran')
-*/
