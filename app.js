bee4b205b2f32f870711b3a90bc3badade0ea7f4
Yonni <yonni.palay@gmail.com>
2020-06-04 11:07:40 +0200

    It works locally?

--- app.js
+++ app.js
@@ -1,29 +1,31 @@
-// const http = require('http')
 const port = process.env.PORT || 3000
-// const port = 3005
+const http = require('http')
+const express = require('express')
+const app = express()
+const mainRoutes = require('./mainRoutes')
 /*
 const server = http.createServer((req, res) => {
   res.statusCode = 200
   res.setHeader('Content-Type', 'text/html')
   res.end('<h1>Hello World</h1>')
 })
-*/
-/*
+
 server.listen(port, () => {
   console.log('Server running at port ' + port)
 })
 */
-const express = require('express')
-const app = express()
-const mainRoutes = require('./mainRoutes')
-
 app.use(mainRoutes)
+var server = app.listen(port)
+server = http.createServer((req, res) => {
+  res.statusCode = 200
+  res.setHeader('Content-Type', 'text/html')
+  res.end('<h1>Hello World</h1>')
+})
+// server.listen(port)
 
 // const port = process.env.PORT || 3000
 // app.listen(port)
-app.listen(port, () => {
-  console.log('Server running at port ' + port)
-})
+
 // console.log('Express server running on port', port)
 
 console.log('index.js ran')
